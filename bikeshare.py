8ab6d086a2b41ee79ebf4517d1a8a4db4309e4cc
simrannsrivastava <srivastavasimran1912@gmail.com>
2024-06-02 23:28:44 +0530

    refactor: update to code

--- bikeshare.py
+++ bikeshare.py
@@ -14,7 +14,8 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!!')
+
 
     while True:
         city = input("Would you like to see data for Chicago, New York, or Washington? ").lower()
